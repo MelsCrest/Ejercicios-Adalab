3f1bbbbd0d67ec164ef4af6644b707e49a0e3cad
MelsCrest <melisacresta@gmail.com>
2024-12-13 09:57:04 +0100

    que vemos esta noche

--- modulo-2/ejercicios-2-2/informacion-eventos/que-ver/main.js
+++ modulo-2/ejercicios-2-2/informacion-eventos/que-ver/main.js
@@ -5,3 +5,18 @@ const eternalSunshineOfTheSM = 'Eternal sunshine of the spotless mind';
 const blueVelvet = 'Blue velvet';
 const split = 'Split';
 
+const btn = document.querySelector('.js-btn');
+const choosenFilm = document.querySelector('.js-selected');
+const moviesList = document.querySelector('.js-list');
+
+btn.addEventListener('click', ()=>{
+    moviesList.innerHTML = `<li>${inception}</li>
+                            <li>${theButterFlyEffect}</li>
+                            <li>${eternalSunshineOfTheSM}</li>
+                            <li>${blueVelvet}</li>
+                            <li>${split}</li>` 
+});
+
+moviesList.addEventListener('click', (event)=>{
+    choosenFilm.innerHTML = 'La peli seleccionada es ' + event.target.innerHTML;
+});
