f6e28be74c95eceb39ca238e5944d6dcb9f0e630
MelsCrest <melisacresta@gmail.com>
2024-12-17 17:24:04 +0100

    arrays

---
+++ modulo-2/ejercicios-2-8/peliculas/main.js
@@ -0,0 +1,10 @@
+'use strict';
+function workWithMovies(){
+const filmList = ["Barbie", "Top Secret", "Horns"];
+filmList[3] = "Cazafantasmas";
+console.log(filmList);
+filmList[1] = "Beetlejuice";
+console.log(filmList);
+};
+
+workWithMovies();
