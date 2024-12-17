d7338212103a7e19da0ff4e7d8fcacbdc17e6a40
MelsCrest <melisacresta@gmail.com>
2024-12-17 22:15:10 +0100

    ejercicio mucho en comun

--- modulo-2/ejercicios-2-9/mucho-en-comun/main.js
+++ modulo-2/ejercicios-2-9/mucho-en-comun/main.js
@@ -2,13 +2,25 @@
 
 const form = document.querySelector('.js-form');
 const btn = document.querySelector('.js-btn');
+let listBooks = [];
 
 
 function handleForm(ev){
     ev.preventDefault();
-    const text = document.querySelector('.js-text');
     const value = ev.target.value;
-    text.innerHTML += `¡A mí también me encantó "${value}"!`;
-}
-form.addEventListener('submit', handleForm);
-btn.addEventListener('click', handleForm);
+    if (listBooks.length === 0){
+        listBooks[0] = value; 
+    }else{
+        listBooks[listBooks.length] = value; 
+    };
+};
+
+function render(){
+    const text = document.querySelector('.js-text');
+    for (const book of listBooks){
+        text.innerHTML += `¡A mí también me encantó "${book}"!`;
+    } 
+};
+
+form.addEventListener('change',handleForm);
+btn.addEventListener('click', render);
