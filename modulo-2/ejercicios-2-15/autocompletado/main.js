eac186c8c9d8500474d75fec93d3d89d53dd90cd
MelsCrest <melisacresta@gmail.com>
2025-01-12 12:58:37 +0100

    ejercicio autocompletado

--- modulo-2/ejercicios-2-15/autocompletado/main.js
+++ modulo-2/ejercicios-2-15/autocompletado/main.js
@@ -12,25 +12,51 @@ const users = [
         telephone: 658927435,
     },
     {
-        name: "Mari Carmen",
+        name: "MariCarmen",
         lastName: "Hernández",
         telephone: 658927435,
     }
 ];
 
 const inputName = document.querySelector('.js-input-name');
-const inputLastNAme = document.querySelector('.js-input-lastname');
+const inputLastName = document.querySelector('.js-input-lastname');
 const inputTel = document.querySelector('.js-input-tel');
 
 const selectUser = document.querySelector('.js-select-user');
-const nameUser = 'Paqui'; 
+
 function handleChange(){
     const selectUserValue = selectUser.value;
     
-    if(selectUserValue === 'pepi'){
-        const newContentName = document.createTextNode(users[0].name);
-        inputName.appendChild(newContentName);
-    }
+    // if(selectUserValue.toLowerCase() === users[0].name.toLowerCase()){
+    //     inputName.setAttribute('value', users[0].name);
+    //     inputLastName.setAttribute('value', users[0].lastName);
+    //     inputTel.setAttribute('value', users[0].telephone);
+    // }else if(selectUserValue.toLowerCase() === users[1].name.toLowerCase()){
+    //     inputName.setAttribute('value', users[1].name);
+    //     inputLastName.setAttribute('value', users[1].lastName);
+    //     inputTel.setAttribute('value', users[1].telephone);
+    // }else if(selectUserValue.toLowerCase() === users[2].name.toLowerCase()){
+    //     inputName.setAttribute('value', users[2].name);
+    //     inputLastName.setAttribute('value', users[2].lastName);
+    //     inputTel.setAttribute('value', users[2].telephone);
+    // }
+    switch(selectUserValue){
+        case 'pepi' : 
+            inputName.setAttribute('value', users[0].name);
+            inputLastName.setAttribute('value', users[0].lastName);
+            inputTel.setAttribute('value', users[0].telephone);
+        break;
+        case 'loli' :
+            inputName.setAttribute('value', users[1].name);
+            inputLastName.setAttribute('value', users[1].lastName);
+            inputTel.setAttribute('value', users[1].telephone);
+        break;
+        case 'maricarmen' : 
+            inputName.setAttribute('value', users[2].name);
+            inputLastName.setAttribute('value', users[2].lastName);
+            inputTel.setAttribute('value', users[2].telephone);
+        break;
+    };
 };
 
 selectUser.addEventListener('change', handleChange);
