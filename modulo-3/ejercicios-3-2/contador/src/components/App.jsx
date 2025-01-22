9b16e970f07c8e4860394633be56141d4c4cc8d4
MelsCrest <melisacresta@gmail.com>
2025-01-22 19:57:46 +0100

    ejercicio el contador - sin finalizar

---
+++ modulo-3/ejercicios-3-2/contador/src/components/App.jsx
@@ -0,0 +1,15 @@
+import '../styles/App.scss'; 
+
+function App() { 
+  
+  return (
+    <>
+      <h2>Contador: 0</h2>
+      <button> + </button>
+      <button> - </button>
+      <button> reset </button>
+    </>
+    )
+}
+
+export default App;
